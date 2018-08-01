618882f8236b9e1292ef863d5105549f04a56610
Axel Fiolle <ax.fiolle@gmail.com>
2018-08-01 19:38:20 +0200

    Component form + Imported @A/material

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  inputValue = 'lorem';
+  newPost = 'lipsum';
+
   onAddPost() {
-    console.log('onAddPost()');
+    console.log('onAddPost()', this.inputValue);
+    this.newPost = this.inputValue;
   }
 }
